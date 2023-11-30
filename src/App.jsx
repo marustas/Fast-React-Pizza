e85553362a77e4432bcaac7d38a3f5549dc4510a
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-30 13:36:30 +0100

    Implemented fetch as render

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu /> },
+      { path: "/menu", element: <Menu />, loader: menuLoader },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:orderId", element: <Order /> },
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,19 @@
-function Menu() {
-  return <h1>Menu</h1>;
-}
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 
+function Menu() {
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu.map((menuItem) => (
+        <MenuItem pizza={menuItem} key={menuItem.id} />
+      ))}
+    </ul>
+  );
+}
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
 export default Menu;
