b71c215861bade43401894d5fe2efc5ec8babb67
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-01 20:15:59 +0100

    Started styling via tailwind

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+const Username = () => {
+  return <div className="text-sm font-semibold">Stas</div>;
+};
+
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">Fast React Pizze Co.</Link>
-      <p>ABABB</p>
+    <header className="bg-yellow-500 uppercase">
+      <Link className="tracking-widest" to="/">
+        Fast React Pizze Co.
+      </Link>
+      <Username />
       <SearchOrder />
     </header>
   );
