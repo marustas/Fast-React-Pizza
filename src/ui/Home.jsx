3e71fd8cbca8928b04335cb1693e3d07066be8d2
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-01 19:48:03 +0100

    Finshed tailwind setup

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1 className="text-xl text-yellow-500 font-semibold text-center">
+      <h1 className="text-center text-xl font-semibold text-yellow-500">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
