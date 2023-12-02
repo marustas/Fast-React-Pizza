837e44d6c77e7d22ede6cf548d90204902ac5325
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-02 18:25:46 +0100

    Applied box model styling

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 const Username = () => {
-  return <div className="text-sm font-semibold">Stas</div>;
+  return <div className="hidden text-sm font-semibold">Stas</div>;
 };
 
 export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 const Header = () => {
   return (
-    <header className="bg-yellow-500 uppercase">
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
       <Link className="tracking-widest" to="/">
         Fast React Pizze Co.
       </Link>
