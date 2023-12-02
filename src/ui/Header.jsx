c597950a3f9eb391c98f240de5cb4551a7829e61
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-02 18:39:52 +0100

    More responsive styling

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 const Header = () => {
   return (
-    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link className="tracking-widest" to="/">
         Fast React Pizze Co.
       </Link>
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,7 +2,7 @@ import CreateUser from "../features/user/CreateUser";
 
 function Home() {
   return (
-    <div className="my-10 text-center">
+    <div className="sm-my:16 my-10 text-center">
       <h1 className="mb-8 text-xl font-semibold">
         The best pizza.
         <br />
