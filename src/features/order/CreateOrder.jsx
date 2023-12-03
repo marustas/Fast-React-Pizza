389591e690b1477b260a4dda4df435f373d4fb57
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-03 18:27:08 +0100

    More usages of user slice

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -39,6 +40,7 @@ function CreateOrder() {
 
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
+  const username = useSelector((store) => store.user.username);
 
   return (
     <div className="px-4 py-6">
@@ -55,6 +57,7 @@ function CreateOrder() {
             type="text"
             name="customer"
             required
+            defaultValue={username}
           />
         </div>
 
