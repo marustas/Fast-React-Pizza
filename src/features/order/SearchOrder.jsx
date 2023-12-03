b148c2b5e7f537766e896becb2b6d4feabdc57cf
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-03 13:20:37 +0100

    Started styilng the order form

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -14,6 +14,7 @@ const SearchOrder = () => {
   return (
     <form onSubmit={handleSubmit}>
       <input
+        className=" tansition-all w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
         placeholder="Search order"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
