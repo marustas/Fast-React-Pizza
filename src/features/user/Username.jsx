9464301e093ec326bdf6909ce218eae2115dbabe
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-03 18:11:17 +0100

    Configured redux store

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+
 const Username = () => {
-  return <div className="hidden text-sm font-semibold md:block">Stas</div>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 };
 
 export default Username;
