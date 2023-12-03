d6c752ef1445ef860197430922da70a37267a37b
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-03 15:13:11 +0100

    Styled the cart component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,6 +7,8 @@ const Button = ({ children, disabled, to, type }) => {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " text-xs py-2 md:px-5 md:py-2.5",
+    secondary:
+      "px-4 py-2.5 md:px-6 md:py-3.5 border-2 border-stone-300 inline-block rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed",
   };
 
   if (to) {
